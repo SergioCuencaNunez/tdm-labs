a99aadd1c2ecea04d7b5ad56bf0820d0eaf8f6ce
Sergio <scuenca06@gmail.com>
2025-03-08 11:48:19 +0100

    sprint3 updated and sprint1 fetch data years corrected

--- labs/project/sprint2/create_tables.sql
+++ labs/project/sprint2/create_tables.sql
@@ -17,7 +17,6 @@ ALTER TABLE gittba04.btcusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/g
 ALTER TABLE gittba04.btcusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/BTCUSDT/year=2022';
 ALTER TABLE gittba04.btcusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/BTCUSDT/year=2023';
 ALTER TABLE gittba04.btcusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/BTCUSDT/year=2024';
-ALTER TABLE gittba04.btcusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/BTCUSDT/year=2025';
  
 -- Repeat for all coins
 CREATE EXTERNAL TABLE IF NOT EXISTS gittba04.ethusdt LIKE gittba04.btcusdt LOCATION 'hdfs:/datos/gittba/gittba04/ETHUSDT';
@@ -46,55 +45,46 @@ ALTER TABLE gittba04.ethusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/g
 ALTER TABLE gittba04.ethusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/ETHUSDT/year=2022';
 ALTER TABLE gittba04.ethusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/ETHUSDT/year=2023';
 ALTER TABLE gittba04.ethusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/ETHUSDT/year=2024';
-ALTER TABLE gittba04.ethusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/ETHUSDT/year=2025';
  year=
 ALTER TABLE gittba04.xrpusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/XRPUSDT/year=2021';
 ALTER TABLE gittba04.xrpusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/XRPUSDT/year=2022';
 ALTER TABLE gittba04.xrpusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/XRPUSDT/year=2023';
 ALTER TABLE gittba04.xrpusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/XRPUSDT/year=2024';
-ALTER TABLE gittba04.xrpusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/XRPUSDT/year=2025';
  year=
 ALTER TABLE gittba04.solusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/SOLUSDT/year=2021';
 ALTER TABLE gittba04.solusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/SOLUSDT/year=2022';
 ALTER TABLE gittba04.solusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/SOLUSDT/year=2023';
 ALTER TABLE gittba04.solusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/SOLUSDT/year=2024';
-ALTER TABLE gittba04.solusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/SOLUSDT/year=2025';
  
 ALTER TABLE gittba04.dogeusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/DOGEUSDT/year=2021';
 ALTER TABLE gittba04.dogeusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/DOGEUSDT/year=2022';
 ALTER TABLE gittba04.dogeusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/DOGEUSDT/year=2023';
 ALTER TABLE gittba04.dogeusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/DOGEUSDT/year=2024';
-ALTER TABLE gittba04.dogeusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/DOGEUSDT/year=2025';
  
 ALTER TABLE gittba04.adausdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/ADAUSDT/year=2021';
 ALTER TABLE gittba04.adausdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/ADAUSDT/year=2022';
 ALTER TABLE gittba04.adausdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/ADAUSDT/year=2023';
 ALTER TABLE gittba04.adausdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/ADAUSDT/year=2024';
-ALTER TABLE gittba04.adausdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/ADAUSDT/year=2025';
  
 ALTER TABLE gittba04.shibusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/SHIBUSDT/year=2021';
 ALTER TABLE gittba04.shibusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/SHIBUSDT/year=2022';
 ALTER TABLE gittba04.shibusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/SHIBUSDT/year=2023';
 ALTER TABLE gittba04.shibusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/SHIBUSDT/year=2024';
-ALTER TABLE gittba04.shibusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/SHIBUSDT/year=2025';
  
 ALTER TABLE gittba04.dotusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/DOTUSDT/year=2021';
 ALTER TABLE gittba04.dotusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/DOTUSDT/year=2022';
 ALTER TABLE gittba04.dotusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/DOTUSDT/year=2023';
 ALTER TABLE gittba04.dotusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/DOTUSDT/year=2024';
-ALTER TABLE gittba04.dotusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/DOTUSDT/year=2025';
  
 ALTER TABLE gittba04.aaveusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/AAVEUSDT/year=2021';
 ALTER TABLE gittba04.aaveusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/AAVEUSDT/year=2022';
 ALTER TABLE gittba04.aaveusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/AAVEUSDT/year=2023';
 ALTER TABLE gittba04.aaveusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/AAVEUSDT/year=2024';
-ALTER TABLE gittba04.aaveusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/AAVEUSDT/year=2025';
  
 ALTER TABLE gittba04.xlmusdt ADD PARTITION (year = 2021) LOCATION 'hdfs:/datos/gittba/gittba04/XLMUSDT/year=2021';
 ALTER TABLE gittba04.xlmusdt ADD PARTITION (year = 2022) LOCATION 'hdfs:/datos/gittba/gittba04/XLMUSDT/year=2022';
 ALTER TABLE gittba04.xlmusdt ADD PARTITION (year = 2023) LOCATION 'hdfs:/datos/gittba/gittba04/XLMUSDT/year=2023';
 ALTER TABLE gittba04.xlmusdt ADD PARTITION (year = 2024) LOCATION 'hdfs:/datos/gittba/gittba04/XLMUSDT/year=2024';
-ALTER TABLE gittba04.xlmusdt ADD PARTITION (year = 2025) LOCATION 'hdfs:/datos/gittba/gittba04/XLMUSDT/year=2025';
 
 -- Check Partitions for all tables
 SHOW PARTITIONS gittba04.btcusdt;
